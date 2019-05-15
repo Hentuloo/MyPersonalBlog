3b26d30a10e0d3e1e124fac3c3c113bc20b22e54
Kamil <Kamil@>
2019-05-15 09:17:49 +0200

    install eslint

---
+++ src/themes/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+*,*::after,*::before{
+  box-sizing:border-box;
+}
+  html{
+    font-size:62.5%;
+  }
+  body {
+   font-size:1.6rem;
+   font-family:Montserrat,sans-serif;
+  }`;
+export default GlobalStyle;
